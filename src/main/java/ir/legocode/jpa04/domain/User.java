c55347f9b89a531002e6bd19856c33aa27c5f73c
fouad <133219919+mhoseinre@users.noreply.github.com>
2023-08-27 15:14:00 +0330

    new  comit

--- src/main/java/ir/legocode/jpa04/domain/User.java
+++ src/main/java/ir/legocode/jpa04/domain/User.java
@@ -1,13 +1,28 @@
 package ir.legocode.jpa04.domain;
 
+import com.sun.xml.bind.v2.runtime.unmarshaller.TagName;
 import ir.legocode.jpa04.base.models.BaseEntity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = User.TABLE_NAME)
 public class User extends BaseEntity<Long> {
-
+//
+    public static final String TABLE_NAME = "user_table";
+    public static final String FIRST_NAME = "first_name";
+    public static final String LAST_NAME = "last_name";
+    public static final String AGE = "age";
+   //
+    @Column(name = User.FIRST_NAME)
     private String firstName;
+    @Column(name = User.LAST_NAME)
     private String lastName;
+    @Column(name = User.AGE)
     private String age;
-
+//
     public User(String firstName, String lastName, String age) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -16,7 +31,7 @@ public class User extends BaseEntity<Long> {
 
     public User() {
     }
-
+//
     public String getFirstName() {
         return firstName;
     }
@@ -39,5 +54,15 @@ public class User extends BaseEntity<Long> {
 
     public void setAge(String age) {
         this.age = age;
+    }
+    //
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age='" + age + '\'' +
+                '}';
     }
 }
